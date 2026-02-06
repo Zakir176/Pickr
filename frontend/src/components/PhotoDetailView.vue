cd6736c9b89747a38438d6185a655b3c2981d84a
Zakir Motala <zakirhusseinmotala76@gmail.com>
2026-02-06 05:01:20 +0200

    Add PhotoDetailView component placeholder

---
+++ frontend/src/components/PhotoDetailView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>Group Detail View (Screen 3)</h1>
+    <p>This is a placeholder for the group detail view.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
