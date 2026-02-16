3440021356667a17947f6d9eadaa8d6d5ddfbc0a
Zakir Motala <zakirhusseinmotala76@gmail.com>
2026-02-16 13:57:50 +0200

    feat: Initialize Vue.js frontend with photo upload, analysis, curation, and PWA support.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueVirtualScroller from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueVirtualScroller)
+app.mount('#app')
